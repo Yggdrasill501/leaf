d892649f0b8f421b990864cebbe84d70a12a5446
Filip Žitný <filipzitny@gmail.com>
2024-09-02 22:35:28 +0200

    crewler test

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -16,7 +16,7 @@ links = [l for l in links if"/squads" in l]
 team_urls = [f"{l}" for l in links]
 
 for team_url in team_urls:
-    team_name = team_url.slit("/")][-1].replace("-Stats","")
+    team_name = team_url.split("/")][-1].replace("-Stats","")
     data = requests.get(team_url).text
     soup = BeautifulSoup(data,"lxml")
     stats = soup.find_all('table', class_ = 'stats_table')[0]
@@ -26,3 +26,8 @@ for team_url in team_urls:
 
     team_data = pd.read_html(str(stats))[0]
     team_data["Teams"] = team_name
+    all_teams.append(team_data)
+    time.sleep(5)
+
+stat_df = pd.concat(all_teams)
+stat_df.to_csv("stats.csv")
